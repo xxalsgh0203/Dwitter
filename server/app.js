c24a8595dbf9264c91c4b9377edb86c4ed924cb4
Minho Kim <music4537@naver.com>
2021-07-06 20:16:14 +0900

    all done

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import 'express-async-errors'
-import tweetsRouter from './router/tweet.js';
+import tweetsRouter from './router/tweets.js';
 
 const app = express();
+
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
@@ -14,11 +15,11 @@ app.use(morgan('tiny'));
 app.use('/tweets', tweetsRouter);
 
 app.use((req, res, next) => {
-    res.sendStatus(404);
+  res.sendStatus(404);
 });
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    res.sendStatus(500);
-})
+  console.error(error);
+  res.sendStatus(500);
+});
 app.listen(8080);
